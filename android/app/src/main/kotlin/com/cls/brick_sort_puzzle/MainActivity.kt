7ea381fa9edcba073d9ab3cdbf243639a0050c3f
Florian Prümer <florianpruemer@gmail.com>
2024-01-15 16:33:40 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/cls/brick_sort_puzzle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cls.brick_sort_puzzle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:brick_sort_puzzle/widgets/brick_stack_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Brick Sort Puzzle',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: BrickStackWidget())),
+    );
+  }
+}
